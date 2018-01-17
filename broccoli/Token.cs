9496efd6793bb4f17d7c80fea4b39b5ce1a3f4f3
Nick Clifford <nick@nickclifford.me>
2018-01-17 12:37:10 -0600

    start on tokenizer for parsing

---
+++ broccoli/Token.cs
@@ -0,0 +1,31 @@
+﻿namespace broccoli
+{
+    public struct Token
+    {
+        public enum TokenType
+        {
+            // Structure
+            LEFT_PAREN, RIGHT_PAREN,
+            
+            // Literals
+            STRING, INTEGER, FLOAT,
+            
+            // Identifiers
+            IDENTIFIER, SCALAR, LIST,
+            
+            // Miscellaneous
+            EOF
+        }
+        
+        public TokenType Type { get; }
+        public string Literal { get; }
+        public int LineNumber { get; }
+
+        public Token(TokenType type, string literal, int line)
+        {
+            Type = type;
+            Literal = literal;
+            LineNumber = line;
+        }
+    }
+}
---
+++ broccoli/Tokenizer.cs
@@ -0,0 +1,19 @@
+﻿namespace broccoli
+{
+    public class Tokenizer
+    {
+        private string source;
+        private Token[] tokens;
+
+        public Tokenizer(string _source, Token[] _tokens)
+        {
+            source = _source;
+            tokens = _tokens;
+        }
+
+        public Token[] scanToken()
+        {
+            // TODO
+        }
+    }
+}
