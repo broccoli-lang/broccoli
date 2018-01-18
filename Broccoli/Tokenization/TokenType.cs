eda9ffa0d04e6c0e85ab572cfab426cc40909495
Nick Clifford <nick@nickclifford.me>
2018-01-18 12:27:02 -0600

    start adding token scanning

--- Broccoli/Tokenization/TokenType.cs
+++ Broccoli/Tokenization/TokenType.cs
@@ -1,17 +1,17 @@
 namespace Broccoli.Tokenization
 {
     public enum TokenType
-        {
-            // Structure
-            LeftParen, RightParen,
-            
-            // Literals
-            String, Integer, Float,
-            
-            // Identifiers
-            Identifier, Scalar, List,
-            
-            // Miscellaneous
-            Eof
-        }
+    {
+        // Structure
+        LeftParen, RightParen,
+        
+        // Literals
+        String, Integer, Float,
+        
+        // Identifiers
+        Identifier, Scalar, List,
+        
+        // Miscellaneous
+        Eof
+    }
 }
--- Broccoli/Tokenization/Tokenizer.cs
+++ Broccoli/Tokenization/Tokenizer.cs
@@ -1,21 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Broccoli.Tokenization
 {
     public class Tokenizer
     {
-        private string source;
-        private Token[] tokens;
+        private string[] source;
+        private uint row = 1;
+        private uint column = 0;
+        private List<Token> tokens = new List<Token>();
 
-        public Tokenizer(string _source, Token[] _tokens)
+        public Tokenizer(string _source)
         {
-            source = _source;
-            tokens = _tokens;
+            source = _source.Split('\n');
         }
 
-        public Token[] scanToken()
+        public void ScanToken()
         {
-            // TODO
-            throw new NotImplementedException();
+            try
+            {
+                switch (NextChar())
+                {
+                    // Single-char tokens
+                    case '(':
+                        tokens.Add(new Token(TokenType.LeftParen, "(", row, column));
+                        break;
+                    case ')':
+                        tokens.Add(new Token(TokenType.RightParen, ")", row, column));
+                        break;
+                    // Variables
+                    case '$':
+                        tokens.Add(new Token(TokenType.Scalar, "$" + NextIdentifier(), row, column));
+                        break;
+                    case '@':
+                        tokens.Add(new Token(TokenType.List, "@" + NextIdentifier(), row, column));
+                        break;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                tokens.Add(new Token(TokenType.Eof, "", row, column));
+            }
+        }
+
+        private char NextChar()
+        {
+            return source[row - 1][(int) column];
+        }
+
+        private void NextLine()
+        {
+            row++;
+            column = 0;
+        }
+
+        private string NextIdentifier()
+        {
+            string result = string.Empty;
+            char c;
+
+            while (!IsValidIdentifier(c = NextChar()))
+            {
+                result += c;
+            }
+
+            return result;
+        }
+
+        private bool IsValidIdentifier(char c)
+        {
+            return c == '_' || !(Char.IsWhiteSpace(c) || Char.IsPunctuation(c));
         }
     }
 }
