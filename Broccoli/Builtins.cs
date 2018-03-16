f5d79b81c3dca1865c6ee7f614def4e287a36002
DJMcMayhem <jimmy.harrington@rocketmail.com>
2018-03-16 00:57:33 -0600

    Added "map" function
    (doesn't yet work for list argument)

--- Broccoli/Builtins.cs
+++ Broccoli/Builtins.cs
@@ -794,6 +794,27 @@ namespace Broccoli {
                         return broccoli.EvaluateExpression(statements.Last());
                     return null;
                 })},
+                {"map", new ShortCircuitFunction("map", -2, (broccoli, args) => {
+                    var func = broccoli.EvaluateExpression(args[0]);
+
+                    IFunction fn = null;
+                    switch (broccoli.EvaluateExpression(func)) {
+                        case Atom a:
+                            fn = broccoli.Scope[a.Value] ?? broccoli.Builtins.GetValueOrDefault(a.Value, null);
+                            if (fn == null)
+                                throw new Exception($"Function {a.Value} does not exist");
+                            break;
+
+                        case AnonymousFunction f:
+                            fn = f;
+                            break;
+                            
+                        default:
+                            throw new Exception($"Received {TypeName(args[0])} instead of atom or lambda in argument 0 for 'map'");
+                    }
+
+                    return new ValueList(args.Skip(1).ToArray().Select(x => fn.Invoke(broccoli, new IValueExpressible[] {x})));
+                })},
             }.Extend(DefaultBuiltins).FluentRemove("call")}
         };
     }
